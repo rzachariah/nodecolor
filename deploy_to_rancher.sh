79cd73b8eb92adc2c5f2d6082bc919184c61c60c
Robert <robzhu@gmail.com>
2015-09-16 16:38:04 -0400

    moving the rancher prep logic out of the circle.yml file

--- deploy_to_rancher.sh
+++ deploy_to_rancher.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO: this file should be obtained via wget from the circle.yml file
 # Performs a rolling upgrade to the target rancher environment
 
-echo $DEPLOY_ON_PASS
+if !$DEPLOY_ON_PASS; then
+    echo "to enable continuous deployment, set the DEPLOY_ON_PASS in the circleCI project settings";
+    exit;
+fi
+
+echo "installing the rancher-upgrade-prep tool"
+git clone https://github.com/robzhu/rancher-upgrade-prep
+cp -a ./rancher-upgrade-prep/. ./
+npm install
+
 echo "Deploying to rancher server: ${RANCHER_URL}"
