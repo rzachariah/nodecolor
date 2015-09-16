d3342a98b26ad2fef1396c335340e7718ea9ea49
Robert Zhu <robzhu@gmail.com>
2015-09-16 11:47:52 -0400

    fixing environment variable eval

--- deploy_to_rancher.sh
+++ deploy_to_rancher.sh
@@ -3,5 +3,5 @@
 # TODO: this file should be obtained via wget from the circle.yml file
 # Performs a rolling upgrade to the target rancher environment
 
-echo DEPLOY_ON_PASS
-echo "Deploying to rancher server: {RANCHER_URL}"
+echo $DEPLOY_ON_PASS
+echo "Deploying to rancher server: ${RANCHER_URL}"
