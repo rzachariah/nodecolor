af6aa99f88e54b623cb157f1efd79e63fc14796c
Robert Zhu <robzhu@gmail.com>
2015-09-16 11:25:05 -0400

    initial checkin for rancher CD script

---
+++ deploy_to_rancher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# TODO: this file should be obtained via wget from the circle.yml file
+# Performs a rolling upgrade to the target rancher environment
+
+echo ${DEPLOY_ON_PASS}
+echo "DEPLOYING TO RANCHER!"
