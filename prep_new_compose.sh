5ad73c61092321df4b5ed2d70fb6d1d3b257ccd2
Robert Zhu <robzhu@gmail.com>
2015-09-17 11:57:42 -0400

    moving back to shell due to circleCI cd limitation

---
+++ prep_new_compose.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO: this file should be obtained via wget from the circle.yml file
+# Performs a rolling upgrade to the target rancher environment
+
+# if ! $DEPLOY_ON_PASS; then
+#     echo "to enable continuous deployment, set the DEPLOY_ON_PASS in the circleCI project settings";
+#     exit;
+# fi
+
+# node main.js docker-compose.yml webspike webspike-39
+
+# echo "Deploying to rancher server: ${RANCHER_URL}"
