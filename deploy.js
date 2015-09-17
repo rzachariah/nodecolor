81164b07d74c7061bf4fe93cb183223f01e30669
Robert Zhu <robzhu@gmail.com>
2015-09-17 10:58:01 -0400

    switching circle.yml to run javascript instead of bash for deployment

---
+++ deploy.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('running js script');
+console.log('deploy: ' + process.env.DEPLOY_ON_PASS);
--- deploy_to_rancher.sh
+++ deploy_to_rancher.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env node
 
 # TODO: this file should be obtained via wget from the circle.yml file
 # Performs a rolling upgrade to the target rancher environment
 
-if ! $DEPLOY_ON_PASS; then
-    echo "to enable continuous deployment, set the DEPLOY_ON_PASS in the circleCI project settings";
-    exit;
-fi
+# if ! $DEPLOY_ON_PASS; then
+#     echo "to enable continuous deployment, set the DEPLOY_ON_PASS in the circleCI project settings";
+#     exit;
+# fi
 
-node main.js docker-compose.yml webspike webspike-39
+# node main.js docker-compose.yml webspike webspike-39
 
-echo "Deploying to rancher server: ${RANCHER_URL}"
+# echo "Deploying to rancher server: ${RANCHER_URL}"
